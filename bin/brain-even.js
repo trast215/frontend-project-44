46b89df1bcee7867886769c4a630cc48037d651e
root <root@DESKTOP-TAUGBQP>
2024-02-12 15:03:59 +0300

    welcome

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import IsEvenGame from '../src/EvenGame.js';
 
-console.log('Welcome to the Brain Games!');
 IsEvenGame();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 const randomQuestion = (defGame) => {
