6332c8871def4b82250a8b693c9295a00afbc299
root <root@DESKTOP-TAUGBQP>
2024-02-15 09:26:36 +0300

    PrimeFix1

--- src/games/PrimeGame.js
+++ src/games/PrimeGame.js
@@ -4,6 +4,9 @@ const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return 'no';
   }
+  if (num === 1) {
+    return 'no';
+  }
   return 'yes';
 };
 const randomQuestionPrime = () => {
